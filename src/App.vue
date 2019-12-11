c5ebdef3933bc9d513003feb34436b694aee174c
isjaki <usow.aleksey@yandex.ru>
2019-12-12 01:06:36 +0300

    create Header, Logo components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <app-header></app-header>
+  </div>
 </template>
 
+<script>
+import Header from './components/Header/Header.vue';
+
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
+</script>
+
 <style lang="scss">
+@import './index.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <app-logo></app-logo>
+    This is Header
+  </div>
+</template>
+
+<script>
+import Logo from '../UI/icons/Logo/Logo.vue';
+
+export default {
+  components: {
+    appLogo: Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../index.scss';
+
+  .header {
+    width: 100%;
+    height: 80px;
+    color: $header-font-color;
+    font-size: 1.25rem;
+    text-transform: uppercase;
+    box-shadow:  0px 10px 12px -3px $shadow-color;
+  }
+</style>
