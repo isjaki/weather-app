17cc7c719d6f9c9f02f2346f5b6c28143dea1722
isjaki <usow.aleksey@yandex.ru>
2020-02-01 22:04:43 +0300

    create svg icon components

--- src/ui/components/icons/SunnyIcon/SunnyIcon.vue
+++ src/ui/components/icons/SunnyIcon/SunnyIcon.vue
@@ -1,3 +1,5 @@
-<svg viewBox="2050 -845 262 262">
+<template>
+  <svg viewBox="2050 -845 262 262">
     <circle cx="131" cy="131" r="131" fill="#ffde17" data-name="Sun Icon" transform="translate(2050 -845)"/>
-</svg>
+  </svg>
+</template>
