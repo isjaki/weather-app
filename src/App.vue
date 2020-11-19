17aff4bae13a1db2db852b167d6c54a009780e31
isjaki <usow.aleksey@yandex.ru>
2020-11-19 23:46:25 +0300

    add CityCard to App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <main>
+      <city-card></city-card>
+      <city-card></city-card>
+      <city-card></city-card>
+    </main>
   </div>
 </template>
 
 <script>
 import Header from '@/ui/widgets/Header/Header.vue';
+import CityCard from '@/ui/widgets/CityCard/CityCard.vue';
 
 export default {
   components: {
     appHeader: Header,
+    CityCard,
   },
 };
 </script>
@@ -20,5 +27,13 @@ export default {
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
+  height: 100%;
+}
+
+main {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: calc(100% - 80px);
 }
 </style>
